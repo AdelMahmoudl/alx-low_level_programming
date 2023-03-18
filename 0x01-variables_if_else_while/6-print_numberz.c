9c3bff9a2de49702b7a14290ee353e6f85eb9439
AdelMahmoudl <dolaabcar6@gmail.com>
2023-03-17 20:01:39 -0700

    a

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+  * main - entry point
+  * description: 'jdjb hcjdc jhcj hjdhc '
+  * Return:always 0
+  */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n + 48);
+	}
+	return (0);
+}
