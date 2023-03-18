719e10d27a20d475650b6450aedc676bf873e615
AdelMahmoudl <dolaabcar6@gmail.com>
2023-03-17 19:51:38 -0700

    a

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+  * main - entry point
+  * description: 'jdjb hcjdc jhcj hjdhc '
+  * Return:always 0
+  */
+int main(void)
+{
+	int n = 97;
+
+	for (n = 0; n <= 9; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
