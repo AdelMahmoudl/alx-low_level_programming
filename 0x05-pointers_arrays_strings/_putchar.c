3069bd54a5e1d35a532977f43d64f3a197f94c1f
AdelMahmoudl <dolaabcar6@gmail.com>
2023-03-27 16:46:09 -0700

    a

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_90 - takes a pointer to an int parameter and updates the value it points to 90
+ *
+ * on: int parameter
+ * Return: Nothing
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
